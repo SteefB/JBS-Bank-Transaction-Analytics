046009b989b392ed61402fb3f0e12eb1f79e4fbd
Jorn Cruijsen <jorncruijsen@gmail.com>
2012-09-24 00:36:13 +0200

    Project folder config

---
+++ src/BankTransactions/Controllers/Charts/GoogleChartDataTableProvider.cs
@@ -0,0 +1,65 @@
+﻿
+using nl.jbs.banktransactions;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BankTransactions.Controllers.Charts
+{
+    public class GoogleChartDataTableProvider
+    {
+        public static string GetChartDataFor(IEnumerable<BankRecord> records)
+        {
+            StringBuilder sb = new StringBuilder("{"
+                         + "cols: ["
+                            + "{id: 'startDate', label: 'Datum', type: 'date'},"
+                            + "{id: 'amount', label: 'Amount', type: 'number'}"
+                        + "],"
+                         + "rows: [");
+
+            foreach(BankRecord rec in records) {
+                //new Date(2008, 1, 26)
+                //sb.Append("{c:[{v: " + rec.Amount + "}, {v: new Date(" + rec.RequestDate.Year + ", " + rec.RequestDate.Month + ", " + rec.RequestDate.Day + "), f:'" + rec.RequestDate.ToShortDateString() + "'}]},");
+                sb.Append(string.Format("{{c:[{{v: new Date({1}, {2}, {3})}}, {{v: {0}}}]}},", 
+                    rec.Amount, 
+                    rec.RequestDate.Year, 
+                    rec.RequestDate.Month, 
+                    rec.RequestDate.Day));
+
+            }
+                        sb.Append("]"
+                    + "}");
+
+            return sb.ToString();
+            
+                            //"{c:[{v: 35}, {v: new Date(2008, 1, 26), f:'February 26, 2008'}]},"
+            return "{"
+                        + "cols: [{id: 'A', label: 'NEW A', type: 'string'},"
+                            + "{id: 'B', label: 'B-label', type: 'number'},"
+                            + "{id: 'C', label: 'C-label', type: 'date'}"
+                        + "],"
+                        + "rows: [{c:[{v: 'a'}, {v: 1.0, f: 'One'}, {v: new Date(2008, 1, 28, 0, 31, 26), f: '2/28/08 12:31 AM'}]},"
+                            + "{c:[{v: 'b'}, {v: 2.0, f: 'Two'}, {v: new Date(2008, 2, 30, 0, 31, 26), f: '3/30/08 12:31 AM'}]},"
+                            + "{c:[{v: 'c'}, {v: 3.0, f: 'Three'}, {v: new Date(2008, 3, 30, 0, 31, 26), f: '4/30/08 12:31 AM'}]}"
+                        + "],"
+                        + "p: {foo: 'hello', bar: 'world!'}"
+                    + "}";
+
+            return "{"
+                        + "cols: [{id: 'A', label: 'NEW A', type: 'string'},"
+                            + "{id: 'B', label: 'B-label', type: 'number'},"
+                            + "{id: 'C', label: 'C-label', type: 'date'}"
+                        + "],"
+                        + "rows: [{c:[{v: 'a'}, {v: 1.0, f: 'One'}, {v: new Date(2008, 1, 28, 0, 31, 26), f: '2/28/08 12:31 AM'}]},"
+                            + "{c:[{v: 'b'}, {v: 2.0, f: 'Two'}, {v: new Date(2008, 2, 30, 0, 31, 26), f: '3/30/08 12:31 AM'}]},"
+                            + "{c:[{v: 'c'}, {v: 3.0, f: 'Three'}, {v: new Date(2008, 3, 30, 0, 31, 26), f: '4/30/08 12:31 AM'}]}"
+                        + "],"
+                        + "p: {foo: 'hello', bar: 'world!'}"
+                    + "}";
+        }
+
+        public static string GetChartOptionsFor(string title, int width, int height)
+        {
+            return string.Format("{{'title': '{0}','width': {1},'height': {2}}}", title, width, height);
+        }
+    }
+}
---
+++ src/BankTransactions/Scripts/Custom/jbs-general.js
@@ -0,0 +1,39 @@
+﻿// TODO: Refactor and un-uglify
+function toggleChartText(caller) {
+    caller.innerHTML = caller.innerHTML == "Draw me a chart..." ? "Hide this chart..." : "Draw me a chart...";
+}
+
+function toggleVisibility(elemId, caller) {
+    caller.innerHTML = caller.innerHTML == "Show more..." ? "Show less..." : "Show more...";
+
+    var rows = document.getElementById(elemId).rows;
+    for (i = 0; i < rows.length; i++) {
+        switchClassName(rows[i], "hide_row", "show_row");
+    }
+}
+
+/**
+* TODO: Refactor into something proper.
+**/
+function switchClassName(elem, a, b) {
+    if (elem.className != undefined && elem.className.indexOf(a) != -1) {
+        elem.className = elem.className.replace(a, b);
+    } else if (elem.className != undefined && elem.className.indexOf(b) != -1) {
+        elem.className = elem.className.replace(b, a);
+    }
+}
+
+$(document).ready(function () {
+    $("#accordion > li > div").click(function () {
+        $("#accordion > li > div").removeClass("opened");
+        $(this).addClass("opened");
+        if (false == $(this).next().is(':visible')) {
+            $('#accordion ul').slideUp(1);
+        }
+        $(this).next().slideToggle(1);
+    });
+    $("#accordion > li > ul > li").click(function () {
+        $("#accordion > li > ul > li").removeClass("strong");
+        $(this).addClass("strong");
+    });
+});
