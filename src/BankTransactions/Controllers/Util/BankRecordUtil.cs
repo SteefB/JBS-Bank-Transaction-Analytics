b7e81b4eaf48b49d7b1a804a594f2ee4b3a7fc9a
Jorn Cruijsen <jorncruijsen@gmail.com>
2012-10-01 01:11:38 +0200

    Added some more files
    - Forgot to do it in the last commit

---
+++ src/BankTransactions/Controllers/Util/BankRecordUtil.cs
@@ -0,0 +1,16 @@
+﻿using nl.jbs.banktransactions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BankTransactions.Controllers.Util
+{
+    public class BankRecordUtil
+    {
+        public static double getAmount(BankRecord record)
+        {
+            return record.Amount * (record.Type.Equals(BankRecord.TransactionType.CREDIT) ? 1 : -1);
+        }
+    }
+}
